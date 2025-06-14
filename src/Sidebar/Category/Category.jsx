d34b81e6c22c2c8404834468a4ac1f2b815e3edf
Tareq Aziz <125204331+tareq159615@users.noreply.github.com>
2025-06-14 18:04:41 +0600

    first commit

---
+++ src/Sidebar/Category/Category.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Category.css"; // Ensure you have the correct path to your CSS file
+import Input from "../../components/Input";
+function Category({ handleChange }) {
+  return (
+    <>
+      <div>
+        <h2 className="sidebar-label-container">Category</h2>
+        <div>
+          <label className="sidebar-label-container">
+            <input onChange={handleChange} type="radio" value="" name="test1" />
+            <span className="checkmark"></span>All
+          </label>
+          <Input
+            handleChange={handleChange}
+            value="sneakers"
+            title="Sneakers"
+            name="test1"
+          />
+          <Input
+            handleChange={handleChange}
+            value="flats"
+            title="Flats"
+            name="test1"
+          />
+          <Input
+            handleChange={handleChange}
+            value="sandals"
+            title="Sandals"
+            name="test1"
+          />
+          <Input
+            handleChange={handleChange}
+            value="heels"
+            title="Heels"
+            name="test1"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Category;
